522bedffd61780840451be11c5492b9ebc52b208
666SIVARAM <7sivaram@gmail.com>
2017-04-05 20:13:58 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# lasttry
+# api.ai_poc
+Hello world business service in webhook
---
+++ app.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import urllib
+import json
+import os
+import MySQLdb
+import sys
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def processRequest(req):
+    if req.get("result").get("action") != "planAlertFinder":
+        return {}
+		
+	return {
+        "speech": "answer",
+        "displayText": "answer",
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+		}
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print "Starting app on port %d" % port
+
+    app.run(debug=False, port=port, host='0.0.0.0')
